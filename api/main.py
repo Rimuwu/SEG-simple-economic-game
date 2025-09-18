d3fceac56599caa39641a5581b31d359ead9ea70
AS1 <arseniykofanov@gmail.com>
2025-09-18 19:31:03 +0300

    -

---
+++ api/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+from contextlib import asynccontextmanager
+
+from global_modules.api_configurate import get_fastapi_app
+
+from global_modules.logs import brain_logger
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    brain_logger.info("API is starting up...")
+    brain_logger.info("Creating missing tables on startup...")
+    yield
+
+    # Shutdown
+
+app = get_fastapi_app(
+    title="API",
+    version="1.0.0",
+    description="Brain API",
+    debug=False,
+    lifespan=lifespan,
+    limiter=False,
+    middlewares=[],
+    routers=[
+    ],
+    api_logger=brain_logger
+)
+# app.add_middleware(RequestLoggingMiddleware, logger=brain_logger)
+
+@app.get("/")
+async def root(request: Request):
+    return {"message": f"{app.description} is running! v{app.version}"}
