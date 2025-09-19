9ed6a230a72ad7a0ea4f5a0d2e07bb721824de71
AS1 <arseniykofanov@gmail.com>
2025-09-20 00:39:55 +0300

    -

--- api/game/user.py
+++ api/game/user.py
@@ -6,10 +6,9 @@ class User(BaseClass):
     __tablename__ = "users"
     __unique_id__ = "user_id"
 
-    user_id: int
-    username: str
-
-    def __init__(self): pass
+    def __init__(self):
+        self.user_id: int = 0
+        self.username: str = ""
 
     def create(self, user_id: int = 0, username: str = ""):
         self.user_id = user_id
