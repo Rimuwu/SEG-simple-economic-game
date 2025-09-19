e7184e7c8e683263586e33cad0b0af17e9b4c3df
AS1 <arseniykofanov@gmail.com>
2025-09-19 18:46:25 +0300

    Создание таблиц и тесты

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 
 from global_modules.api_configurate import get_fastapi_app
 from global_modules.logs import main_logger
+from modules.json_database import just_db
 
 # Импортируем роуты
 from routers import connect_ws
@@ -12,6 +13,19 @@ async def lifespan(app: FastAPI):
     # Startup
     main_logger.info("API is starting up...")
     main_logger.info("Creating missing tables on startup...")
+
+    just_db.create_table('sessions') # Таблица сессий
+    just_db.create_table('users') # Таблица пользователей
+    just_db.create_table('companies') # Таблица компаний
+    just_db.create_table('game_history') # Таблица c историей ходов
+    just_db.create_table('time_schedule') # Таблица с задачами по времени
+    just_db.create_table('step_schedule') # Таблица с задачами по шагам
+    just_db.create_table('contracts') # Таблица с контрактами
+    just_db.create_table('cities') # Таблица с городами
+    just_db.create_table('exchange') # Таблица с биржей
+    just_db.create_table('factories') # Таблица с заводами
+    just_db.create_table('warehouse') # Таблица с складом
+
     yield
 
     # Shutdown
@@ -33,3 +47,14 @@ app = get_fastapi_app(
 @app.get("/")
 async def root(request: Request):
     return {"message": f"{app.description} is running! v{app.version}"}
+
+
+from game.user import User
+
+user = User().create(user_id=123, username="TestUser")
+user.save_to_base()
+
+from modules.json_database import just_db
+
+res = just_db.find_one('users', user_id=123, to_class=User)
+print(res)
