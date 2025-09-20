19e523656bf504ed0838cf45afb9971926d249a3
AS1 <arseniykofanov@gmail.com>
2025-09-20 18:12:25 +0300

    Создание компании и добавление пользователей

--- api/routers/ws_company.py
+++ api/routers/ws_company.py
@@ -1,3 +1,6 @@
+from game.user import User
+from modules import websocket_manager
+from modules import check_password
 from modules.ws_hadnler import message_handler
 from modules.json_database import just_db
 
@@ -56,3 +59,88 @@ async def handle_get_company(client_id: str, message: dict):
                          **{k: v for k, v in conditions.items() if v is not None})
 
     return company
+
+@message_handler(
+    "create-company", 
+    doc="Обработчик создания компании. Отправляет ответ на request_id. Требуется пароль для взаимодействия.",
+    datatypes=[
+        "name: str",
+        "who_create: int",
+
+        "password: str",
+        "request_id: str"
+    ],
+    messages=["api-create_company (broadcast)"]
+)
+async def handle_create_company(client_id: str, message: dict):
+    """Обработчик создания компании"""
+
+    password = message.get("password")
+    name = message.get("name")
+    who_create = message.get("who_create")
+
+    for i in [name, password, who_create]:
+        if i is None: return {"error": "Missing required fields."}
+
+    try:
+        check_password(password)
+        
+        user = User(_id=who_create).reupdate()
+        if not user: raise ValueError("User not found.")
+        company = user.create_company(name=name)
+
+    except ValueError as e:
+        return {"error": str(e)}
+
+    data = {
+        'session_id': company.session_id,
+        'company': company.__dict__
+    }
+
+    await websocket_manager.broadcast({
+        "type": "api-create_company",
+        "data": data
+    })
+    return data
+
+
+@message_handler(
+    "update-company-add-user", 
+    doc="Обработчик обновления компании. Требуется пароль для взаимодействия.",
+    datatypes=[
+        "user_id: int",
+        "company_id: str",
+
+        "password: str"
+    ],
+    messages=["api-update-company-add-user (broadcast)"]
+)
+async def handle_update_company_add_user(client_id: str, message: dict):
+    """Обработчик обновления компании"""
+
+    password = message.get("password")
+    user_id = message.get("user_id")
+    company_id = message.get("company_id")
+
+    for i in [user_id, company_id, password]:
+        if i is None: return {"error": "Missing required fields."}
+
+    try:
+        check_password(password)
+
+        user = User(_id=user_id).reupdate()
+        if not user: raise ValueError("User not found.")
+        company = user.add_to_company(company_id=company_id)
+
+    except ValueError as e:
+        return {"error": str(e)}
+
+    data = {
+        'company_id': company.id,
+        'user_id': user.id
+    }
+
+    await websocket_manager.broadcast({
+        "type": "api-update-company-add-user",
+        "data": data
+    })
