3b0f520c7d1c60bd66c7b6ed8de0cf995eee66ee
AS1 <arseniykofanov@gmail.com>
2025-09-21 18:47:17 +0300

    Сообщение при создании пользователя. Реализация подключения к компании через секретный код. Оповещение при вступлении в компанию и выхода из компании ( с удалением компании если в ней не осталось пользователей

--- api/game/user.py
+++ api/game/user.py
@@ -35,6 +35,14 @@ class User(BaseClass):
 
         self.save_to_base()
         self.reupdate()
+
+        asyncio.create_task(websocket_manager.broadcast({
+            "type": "api-create_user",
+            "data": {
+                'session_id': self.session_id,
+                'user': self.__dict__
+            }
+        }))
         return self
 
     def create_company(self, name: str):
@@ -56,14 +64,15 @@ class User(BaseClass):
         self.reupdate()
         return company
 
-    def add_to_company(self, company_id: int):
+    def add_to_company(self, secret_code: int):
         from game.company import Company
         if self.company_id != 0:
             raise ValueError("User is already in a company.")
 
-        company = Company(_id=company_id).reupdate()
-        if not company:
-            raise ValueError(f"Company with id '{company_id}' does not exist.")
+        company = just_db.find_one("companies", 
+                    to_class=Company, secret_code=secret_code)
+        if not company: 
+            raise ValueError("Company with this secret code not found.")
 
         if company.can_user_enter() is False:
             raise ValueError("Company is not accepting new users at the moment.")
@@ -71,6 +80,14 @@ class User(BaseClass):
         self.company_id = company.id
         self.save_to_base()
         self.reupdate()
+
+        asyncio.create_task(websocket_manager.broadcast({
+            "type": "api-user_added_to_company",
+            "data": {
+                "company_id": self.id,
+                "user_id": self.id
+            }
+        }))
         return company
 
     def delete(self):
@@ -82,4 +99,29 @@ class User(BaseClass):
                 "user_id": self.id
             }
         }))
+        return True
+
+    def leave_from_company(self):
+        from game.company import Company
+        if self.company_id == 0:
+            raise ValueError("User is not in a company.")
+
+        old_company_id = self.company_id
+        self.company_id = 0
+        self.save_to_base()
+        self.reupdate()
+
+        company = Company(_id=old_company_id).reupdate()
+
+        asyncio.create_task(websocket_manager.broadcast({
+            "type": "api-user_left_company",
+            "data": {
+                "company_id": old_company_id,
+                "user_id": self.id
+            }
+        }))
+
+        if company and len(company.users) == 0:
+            company.delete()
+
         return True
