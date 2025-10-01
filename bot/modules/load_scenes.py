f83c2f00fec0d151178fdd3bbd0660f82e2be986
AS1 <arseniykofanov@gmail.com>
2025-10-01 17:02:51 +0300

    фиксы и загрузка из базы сцен

---
+++ bot/modules/load_scenes.py
@@ -0,0 +1,19 @@
+from typing import Type
+from oms import scene_manager
+from oms.manager import SceneManager
+from modules.db import db
+from bot_instance import bot
+
+def load_scenes_from_db(manager: SceneManager):
+
+    results = db.find('scenes')
+    for result in results:
+        manager.load_scene_from_db(
+            result['user_id'],
+            result['scene_path'],
+            result['page'],
+            result['message_id'],
+            result['data'],
+            bot,
+            True
+        )
