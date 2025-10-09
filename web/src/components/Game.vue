cc4e9700d986ed2cd9be1b2b67f09becdc4a0b42
Artem Dubrov <GithubLyagva@gmail.com>
2025-10-09 21:25:15 +0300

    Редизайн страницы с игрой

--- web/src/components/Game.vue
+++ web/src/components/Game.vue
@@ -76,25 +76,54 @@ onUnmounted(() => {
     <div class="right">
       <div class="grid">
         
-        <div class="cities">
+        <div class="cities grid-item">
           <p class="title">ГОРОДА</p>
           <div class="content">
+          <span>
+Город А (ЛВ)<br/>
+&nbsp;&nbsp;• Генератор<br/>
+&nbsp;&nbsp;• Медикаменты<br/>
+<br/>
+Город Б (НЛ)<br/>
+&nbsp;&nbsp;• Генератор<br/>
+&nbsp;&nbsp;• Медикаменты<br/>
+          </span>
+
+          <span>
+Город В (ПВ)<br/>
+&nbsp;&nbsp;• Генератор<br/>
+&nbsp;&nbsp;• Медикаменты<br/>
+<br/>
+Город Г (НП)<br/>
+&nbsp;&nbsp;• Генератор<br/>
+&nbsp;&nbsp;• Медикаменты<br/>
+          </span>
           </div>
         </div>
 
-        <div class="stock">
+        <div class="stock grid-item">
           <p class="title">БИРЖА</p>
           <div class="content">
+            <span>Компания А выставила на продажу продукт Б</span>
+            <span>Компания Б выкупила Х товара у компании С</span>
+            <span>Компания А выставила на продажу продукт Б</span>
+            <span>Компания А выставила на продажу продукт Б</span>
           </div>
         </div>
-        <div class="upgrades">
+        <div class="upgrades grid-item">
           <p class="title">УЛУЧШЕНИЯ</p>
           <div class="content">
+            <span>Компания А улучшила своё хранилище до уровня 2</span>
+            <span>Компания А улучшила своё хранилище до уровня 2</span>
+            <span>Компания А улучшила своё хранилище до уровня 2</span>
+            <span>Компания А улучшила своё хранилище до уровня 2</span>
           </div>
         </div>
-        <div class="contracts">
+        <div class="contracts grid-item">
           <p class="title">КОНТРАКТЫ</p>
           <div class="content">
+            <span>Компания А создала свободный контракт на Х продукта Б на С ходов</span>
+            <span>Компания А создала свободный контракт на Х продукта Б на С ходов</span>
           </div>
         </div>
 
@@ -153,14 +182,48 @@ onUnmounted(() => {
   grid-template-rows: 47.5% 47.5%;
 }
 
+.grid-item {
+  width: 100%; height: 100%;
+  /* background: #0f0; */
+}
+
+.content {
+  font-size: 2.25rem;
+  color: white;
+  font-weight: 400;
+}
+
 .title {
-  font-size: 2.5rem;
+  font-size: 4rem;
   margin: 0;
+  margin-bottom: 10px;
   text-transform: uppercase;
   margin-bottom: 20px;
   color: white;
+  text-align: center;
 }
 
+.cities .content {
+  padding: 5px 10px;
+
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  text-align: left;
+  line-height: 1.5;
+  background: #3D8C00;
+}
+
+.stock .content, .upgrades .content, .contracts .content {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.stock span, .upgrades span, .contracts span {
+  background: #3D8C00;
+  padding: 5px 10px;
+}
 
 .footer {
   width: 90%;
