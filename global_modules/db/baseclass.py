101eac18b74d387fff4d836cf11ec64215cd8844
AS1 <arseniykofanov@gmail.com>
2025-10-15 13:08:31 +0300

    Переход на mongodb и чистка. Веб админка

--- global_modules/db/baseclass.py
+++ global_modules/db/baseclass.py
@@ -1,58 +1,48 @@
 from typing import Optional
-from global_modules.db.json_database import JSONDatabase
+from global_modules.db.mongo_database import MongoDatabase
 
 class BaseClass:
-    """ Базовый класс для всех классов, которые будут сохраняться в JSONDatabase
+    """ Базовый класс для всех классов, которые будут сохраняться в базе данных.
     """
     __tablename__: str = "base" # Имя таблицы в базе данных
-    __unique_id__: str = "id"  # Поле, которое будет использоваться как уникальный идентификатор
-    __db_object__: JSONDatabase  # Экземпляр JSONDatabase, должен быть установлен в подклассе
+    __unique_id__: str = "_id"  # Поле, которое будет использоваться как уникальный идентификатор
+    __db_object__: MongoDatabase  # Экземпляр MongoDatabase, должен быть установлен в подклассе
 
-    def load_from_base(self, data: Optional[dict]):
+    async def load_from_base(self, data: Optional[dict]):
         """ Загружает данные из словаря в атрибуты объекта.
         """
         if data is None: return None
         for key, value in data.items(): setattr(self, key, value)
 
-    def save_to_base(self):
+    async def save_to_base(self):
         """ Сохраняет текущие атрибуты объекта в базу данных.
         """
         # Фильтруем данные, исключая атрибуты, начинающиеся с _
         data_to_save = {key: value for key, value in self.__dict__.items() if not key.startswith('_')}
 
-        if self.__db_object__.find_one(self.__tablename__, 
-                **{self.__unique_id__: 
-                    self.__dict__[self.__unique_id__]}
-                            ) is None:
-            self.__db_object__.insert(self.__tablename__, data_to_save)
-            return
-
-        self.__db_object__.update(self.__tablename__, 
-                {self.__unique_id__: 
-                    self.__dict__[self.__unique_id__]},
+        await self.__db_object__.update(self.__tablename__, 
+                {self.__unique_id__: self.__dict__[self.__unique_id__]},
                 data_to_save
-                       )
+                )
 
-    def reupdate(self):
+    async def insert(self):
+        """ Вставляет текущие атрибуты объекта в базу данных.
+        """
+        # Фильтруем данные, исключая атрибуты, начинающиеся с _
+        data_to_save = {key: value for key, value in self.__dict__.items(
+            ) if not key.startswith('_')}
+
+        await self.__db_object__.insert(self.__tablename__, data_to_save)
+
+    async def reupdate(self):
         """ Обновляет атрибуты объекта из базы данных.
         """
-        self.load_from_base(
-            self.__db_object__.find_one(self.__tablename__, 
-                **{self.__unique_id__: 
-                    self.__dict__[self.__unique_id__]}
-                            )
+        await self.load_from_base(
+            await self.__db_object__.find_one(self.__tablename__, 
+                **{self.__unique_id__: self.__dict__[self.__unique_id__]}
+                ) # type: ignore
         )
         return self
 
     def __repr__(self):
         return f"<{self.__class__.__name__}({self.__dict__})>"
-
-    def update(self, **kwargs):
-        """ Обновляет атрибуты объекта из переданных ключевых словарей.
-        """
-        for key, value in kwargs.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-        self.save_to_base()
-        self.reupdate()
-        return self
