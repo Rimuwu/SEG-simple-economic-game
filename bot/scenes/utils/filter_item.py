fd3d21143661c1bd2acccbe47cf0a48e565b4cfd
Redricks <rodionovm18@gmail.com>
2025-10-22 16:56:00 +0300

    непроверенная версия фильтра предметов

--- bot/scenes/utils/filter_item.py
+++ bot/scenes/utils/filter_item.py
@@ -6,35 +6,40 @@ from oms.utils import callback_generator
 RESOURCES: Resources = ALL_CONFIGS["resources"]
 
 class ItemFilter(OneUserPage):
+    
+    async def data_preparate(self):
+        self.scene.update_key("scene", "page_filter_item", 0)
+    
     async def content_worker(self):
-        return ""
+        page = self.scene.get_key("scene", "page_filter_item")
+        return f"Выберите нужный ресурс ниже:\n Текущая страница: {page + 1}/4"
     
     
     async def buttons_worker(self):
         self.row_width = 3
         buttons = []
-        k = 0
+        page_buttons = []
+        container = []
+        page_container = 0
+        cur_page = self.scene.get_key("scene", "page_filter_item")
         for key, res in RESOURCES.get_raw_resources().items():
-            if k == 1:
-                buttons.append({
-                    "text": f"{res.emoji} {res.label}",
-                    "callback_data": callback_generator(
-                    self.scene.__scene_name__,
-                    "item_select",
-                    key
-                    ),
-                    "next_line": True
-                    })
-                k = 0
-            else:
-                k += 1
-                buttons.append({
-                    "text": f"{res.emoji} {res.label}",
-                    "callback_data": callback_generator(
-                    self.scene.__scene_name__,
-                    "item_select",
-                    key
-                    )})
+            container.append({
+                "text": f"{res.emoji} {res.label}",
+                "callback_data": callback_generator(
+                self.scene.__scene_name__,
+                "item_select",
+                key
+                ),
+                "next_line": True
+                })
+            page_container += 1
+            if page_container == 4:
+                page_container = 0
+                page_buttons.append(container)
+                container = []
+
+        for i in page_buttons[cur_page]:
+            buttons.append(i)
         
         buttons.append({
             "text": "⬅️ Назад",
@@ -45,9 +50,32 @@ class ItemFilter(OneUserPage):
             "ignore_row": True
             })
         buttons.append({
+            "text": f"{cur_page + 1}/{len(page_buttons)}",
+            "callback_data": "ignore"
+        })
+        buttons.append({
             "text": "Вперёд ➡️",
             "callback_data": callback_generator(
                 self.scene.__scene_name__,
                 "next_page"
             )})
         return buttons
+
+    @OneUserPage.on_callback("next_page")
+    async def next_page(self):
+        cur_page = self.scene.get_key("scene", "page_filter_item")
+        if cur_page + 1 > 3:
+            self.scene.update_key("scene", "page_filter_item", 0)
+        else:
+            self.scene.update_key("scene", "page_filter_item", cur_page + 1)
+        await self.scene.update_message()
+    
+    
+    @OneUserPage.on_callback("back_page")
+    async def back_page(self):
+        cur_page = self.scene.get_key("scene", "page_filter_item")
+        if cur_page - 1 < 0:
+            self.scene.update_key("scene", "page_filter_item", 3)
+        else:
+            self.scene.update_key("scene", "page_filter_item", cur_page - 1)
+        await self.scene.update_message()
