e13e1f627695345955f15d0c2433dea67bc60a96
Redricks <rodionovm18@gmail.com>
2025-10-30 11:57:44 +0300

    не протестированная страница main биржи

--- bot/scenes/exchange_page/exchange_main.py
+++ bot/scenes/exchange_page/exchange_main.py
@@ -2,6 +2,8 @@ from utils.oneuser_page import OneUserPage
 from modules.ws_client import get_exchanges
 from global_modules import Resources, ALL_CONFIGS
 from oms.utils import callback_generator
+from aiogram.types import CallbackQuery
+import json
 
 
 RESOURCES: Resources = ALL_CONFIGS["resources"]
@@ -59,14 +61,57 @@ class ExchangeMain(OneUserPage):
                         "callback_data": callback,
                         "next_line": True
                     })
-        
         if len(all_ex_page_container) != 0 and state == "all":
-            buttons.append(*all_ex_page_container)
+            buttons.extend(all_ex_page_container[cur_page*5:cur_page*5+5])
         elif len(our_ex_page_container) != 0 and state == "our":
-            buttons.append(*our_ex_page_container)
-        
+            buttons.extend(our_ex_page_container[cur_page*5:cur_page*5+5])
+
         if len(buttons) != 0:
             buttons.append({"text": "Назад",
-                            "callback_data": callback_generator(self.scene.__scene_name__, "back_page")})
+                            "callback_data": callback_generator(self.scene.__scene_name__, "back_page"),
+                            })
+            list_butt = len(all_ex_page_container) if state == "all" else len(our_ex_page_container)
+            buttons.append({"text": f"{cur_page}/{(list_butt//5 +1) if list_butt/5 > list_butt//5 else list_butt//5}",
+                            "callback_data": callback_generator(self.scene.__scene_name__, "nope"),
+                            })
+            await self.scene.update_key("exchange-main-page", "max_page", (list_butt//5 +1) if list_butt/5 > list_butt//5 else list_butt//5)
+            buttons.append({"text": "Вперёд",
+                            "callback_data": callback_generator(self.scene.__scene_name__, "next_page"),
+                            })
+            buttons.append({"text": f"{'Все предложения' if state == 'all' else 'Ваши предложения'}",
+                            "callback_data": callback_generator(self.scene.__scene_name__, "change_state"),
+                            })
         
         return buttons
+    
+    @OneUserPage.on_callback("select_exchange")
+    async def select_exchange(self, call: CallbackQuery, args: list):
+        await self.scene.update_key("exchange-select-confirm-page", "selected_exchange", args[1])
+        await self.scene.update_page("exchange-select-confirm-page")
+    @OneUserPage.on_callback("change_state")
+    async def change_state(self, call: CallbackQuery, args: list):
+        state = self.scene.get_key("exchange-main-page", "state")
+        if state == "all":
+            await self.scene.update_key("exchange-main-page", "state", "our")
+        else:
+            await self.scene.update_key("exchange-main-page", "state", "all")
+        await self.scene.update_key("exchange-main-page", "page_number", 0)
+        await self.scene.update_message()
+    @OneUserPage.on_callback("next_page")
+    async def next_page(self, call: CallbackQuery, args: list):
+        cur_page = await self.scene.get_key("exchange-main-page", "page_number")
+        max_page = await self.scene.get_key("exchange-main-page", "max_page")
+        if cur_page + 1 <= max_page:
+            await self.scene.update_key("exchange-main-page", "page_number", cur_page + 1)
+        else:
+            await self.scene.update_key("exchange-main-page", "page_number", 0)
+        await self.scene.update_message()
+    @OneUserPage.on_callback("back_page")
+    async def back_page(self, call: CallbackQuery, args: list):
+        cur_page = await self.scene.get_key("exchange-main-page", "page_number")
+        max_page = await self.scene.get_key("exchange-main-page", "max_page")
+        if cur_page - 1 >= 0:
+            await self.scene.update_key("exchange-main-page", "page_number", cur_page - 1)
+        else:
+            await self.scene.update_key("exchange-main-page", "page_number", max_page)
+        await self.scene.update_message()
