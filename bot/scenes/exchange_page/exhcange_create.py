76bc18bb6b98770c957ed691054f96fd954b5342
Redricks <rodionovm18@gmail.com>
2025-11-01 14:23:15 +0300

    кнопки в создании биржи

--- bot/scenes/exchange_page/exhcange_create.py
+++ bot/scenes/exchange_page/exhcange_create.py
@@ -55,6 +55,37 @@ class ExchangeCreate(ItemFilter):
     async def buttons_worker(self):
         data = await self.scene.get_key("exchange-create-page", "settings")
         settings = json.loads(data)
+        sell_resource = "Не выбран"
+        barter_resource = "Не выбрано"
+        sell_amount_per_trade = settings["sell_amount_per_trade"] if settings["sell_amount_per_trade"] else "N"
+        price = settings["price"] if settings["price"] else "N"
+        if settings["sell_resource"] is not None:
+            res1 = RESOURCES.get_resource(settings["sell_resource"])
+            sell_resource = f"{res1.emoji} {res1.label}"
+        if settings["barter_resource"] is not None:
+            res2 = RESOURCES.get_resource(settings["barter_resource"])
+            barter_resource = f"{res2.emoji} {res2.label}"
         self.row_width = 2
         buttons = []
-        buttons.append({"text": f""})
+        buttons.append({"text": f"Ресурс для продажи: {sell_resource} x{sell_amount_per_trade}",
+                         "callback_data": callback_generator(self.scene.__scene_name__, "set_sell_resource"),
+                         "next_line": True})
+        buttons.append({"text": f"{'За монеты' if settings['offer_type']=='money' else 'Бартер'}",
+                        "callback_data": callback_generator(self.scene.__scene_name__, "change_offer_type")})
+        buttons.append({"text": f"Кол-во сделок: {settings['count_offers'] if settings['count_offers'] else 'N'}",
+                        "callback_data": callback_generator(self.scene.__scene_name__, "set_count_offers"),})
+        if settings["offer_type"] == "money":
+            buttons.append({"text": f"Цена за сделку: {price}",
+                            "callback_data": callback_generator(self.scene.__scene_name__, "change_price")})
+        else:
+            buttons.append({"text": f"Ресурс для бартера: {barter_resource} x{settings['barter_amount'] if settings['barter_amount'] else 'N'}",
+                            "callback_data": callback_generator(self.scene.__scene_name__, "set_barter_resource")})
+        buttons.append({"text": "Создать предложение",
+                        "callback_data": callback_generator(self.scene.__scene_name__, "create_exchange_offer"),
+                        "ignore_row": True})
+        buttons.append({"text": "Назад",
+                        "callback_data": callback_generator(self.scene.__scene_name__, "go_back"),
+                        "ignore_row": True})
+        return buttons
+        
+        
